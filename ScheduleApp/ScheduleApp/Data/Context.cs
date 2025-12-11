a99930548c4faea6e54f4f66d34fd9560cbaada9
imStydent <artemmo12309@gmail.com>
2025-12-11 13:25:30 +0300

    Update Context.cs

--- ScheduleApp/ScheduleApp/Data/Context.cs
+++ ScheduleApp/ScheduleApp/Data/Context.cs
@@ -30,7 +30,7 @@ public partial class Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=127.0.0.1;database=schedulebd;user id=root;password=root");
+        => optionsBuilder.UseMySQL("server=mysql;database=ispp2102;user id=2102;password=2102");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
